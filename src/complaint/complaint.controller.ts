7d7a1f54a35877dd4d9a74a785712b9f52cd91ac
Joao Afonso Katombela <ja3328173@gmail.com>
2025-01-03 14:30:45 +0100

    fix: src/complaint/complaint.controller.ts. Modificação de código em src/complaint/complaint.controller.ts

--- src/complaint/complaint.controller.ts
+++ src/complaint/complaint.controller.ts
@@ -1,14 +1,16 @@
 /* eslint-disable prettier/prettier */
 
-import { Controller, Post, Body, Get, Query, Param, Put, Delete } from "@nestjs/common";
+import { Controller, Post, Body, Get, Query, Param, Put, Delete, UseGuards } from "@nestjs/common";
 import { ComplaintService } from "./complaint.service";
 import { CreateComplaintDto, UpdateComplaintDto } from './dto/complaint.dto';
+import { AuthGuard } from "src/auth/auth.guard";
 
 @Controller('api/complaints')
 export class ComplaintController {
     constructor(private readonly complaintService: ComplaintService) { }
 
     @Post()
+    @UseGuards(AuthGuard)
     async create(@Body() createComplaintDto: CreateComplaintDto) {
         return this.complaintService.createCompany({
             ...createComplaintDto,
@@ -19,11 +21,13 @@ export class ComplaintController {
     }
 
     @Get()
+    @UseGuards(AuthGuard)
     async findAll(@Query() query: any): Promise<any[]> {
         return this.complaintService.findAll(query);
     }
 
     @Get(':id')
+    @UseGuards(AuthGuard)
     async findOne(@Param('id') id: string) {
         return this.complaintService.findOne({ id }, {
             include: {
@@ -34,11 +38,13 @@ export class ComplaintController {
     }
 
     @Put(':id')
+    @UseGuards(AuthGuard)
     async update(@Param('id') id: string, @Body() updateComplaintDto: UpdateComplaintDto) {
         return this.complaintService.update({ id }, updateComplaintDto);
     }
 
     @Delete(':id')
+    @UseGuards(AuthGuard)
     async remove(@Param('id') id: string) {
         return this.complaintService.delete({ id });
     }
