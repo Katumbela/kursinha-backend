870739f9ca28be678dbbc24dbee22886a5001358
Joao Afonso Katombela <ja3328173@gmail.com>
2025-02-24 10:21:41 +0100

    fix: fix cors

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,15 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS para localhost, zuela.pt, zuela.vercel.app
+  app.enableCors({
+    origin: "*",
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Métodos permitidos
+    allowedHeaders: 'Content-Type, Authorization', // Cabeçalhos permitidos
+    credentials: false, // Não envia cookies ou credenciais, mas pode ser ajustado conforme necessário
+  });
+
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(4000);
