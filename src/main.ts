fbafd3593793bcb2e8be3b840fe002a5657fcfde
Joao Afonso Katombela <ja3328173@gmail.com>
2025-02-24 11:08:00 +0100

    fix: increase request limit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform/transform.interceptor';
 import { HttpExceptionFilter } from './http/http.filter';
+import * as bodyParser from 'body-parser';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
@@ -11,6 +12,11 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Aumenta o limite do JSON para 100MB (ou outro valor necessário)
+  app.use(bodyParser.json({ limit: '100mb' }));
+  app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+
+
   // Habilitar CORS para localhost, zuela.pt, zuela.vercel.app
   app.enableCors({
     origin: "*",
@@ -18,6 +24,7 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Authorization', // Cabeçalhos permitidos
     credentials: false, // Não envia cookies ou credenciais, mas pode ser ajustado conforme necessário
   });
+
 
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
